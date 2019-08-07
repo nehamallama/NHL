65a828fc18d08d92b92161deeb6f4075e4173972
jacobnehama <j_nehama@coloradocollege.edu>
2019-08-06 22:25:30 -0600

    boilerplate header/navbar completed

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import {
+    Navbar, Form, InputGroup, FormControl,Button, Nav, NavDropdown
+
+
+} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap-theme.css';
+
+const Header = () => (
+
+  <header>
+      <Navbar className="bg-light justify-content-between" >
+          <Form inline>
+              <FormControl type="text" placeholder="Search" className=" mr-sm-2" />
+              <Button type="submit">Submit</Button>
+          </Form>
+          <Form inline adjustsFontSizeToFit={true} >
+              {' NHL'}
+
+          </Form>
+
+          <Form inline>
+                  <Button type="submit">Login</Button>
+          </Form>
+      </Navbar>
+  </header>
+);
+
+export default Header;
